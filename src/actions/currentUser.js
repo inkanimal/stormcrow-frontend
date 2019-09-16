047e53e0f3de783f434397024701d45b6e951196
inkanimal <theatomproject@gmail.com>
2019-09-16 12:29:11 -0400

    Update currentUser.js

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,3 +1,5 @@
+
+
 export const setCurrentUser = user => {
     return {
         type: "SET_CURRENT_USER",
@@ -6,14 +8,26 @@ export const setCurrentUser = user => {
 }
 
 
-export const login = credentials => {
+export const login = (credentials, history) => {
     return dispatch => {
         return fetch("http://localhost:3001/api/v1/login", {
+            credentials: "include",
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(credentials)
         })
+        .then(resp => resp.json())
+        .then(resp => {
+            if(resp.error) {
+              alert(resp.error)
+            } else {
+                dispatch(setCurrentUser(resp.data))
+                history.push('/')
+
+            }
+        })
+        .catch(console.log)
     }
 }
