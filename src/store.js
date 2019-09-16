aa14c0f766141ae288cf18e4f730949adac6637c
inkanimal <theatomproject@gmail.com>
2019-09-15 22:24:54 -0400

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import currentUser from './reducers/currentUser';
+import loginForm from './reducers/loginForm';
 
 
 const reducer = combineReducers({
-   currentUser 
+   currentUser,
+   loginForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
