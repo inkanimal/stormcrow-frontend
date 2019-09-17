dc8150209bc70aed98fb4ad5904de1b77eb31959
inkanimal <theatomproject@gmail.com>
2019-09-17 14:29:58 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Login from "./components/Login";
 import Logout from "./components/Logout";
+// import { getCurrentUser } from "./actions/currentUser";
 
 
 const Navbar = ({ logout }) => {
  
     return (
-       
+       <div>
+
+       </div>
     )
 }
 
-export default connect(null, { logout} )(Logout)
+const mapStateToProps = ({ currentUser }) => {
+    return {
+      currentUser
+    }
+  }
+
+export default connect(null, { logout} )(Navbar)
