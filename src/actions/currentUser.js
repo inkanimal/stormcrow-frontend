4650e12d64c8a5309344711b62d9f1e177dfc763
inkanimal <theatomproject@gmail.com>
2019-09-17 10:22:31 -0400

    add clear current user to reducer and update current user component

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -7,6 +7,12 @@ export const setCurrentUser = user => {
     }
 }
 
+export const clearCurrentUser = user => {
+    return {
+        type: "CLEAR_CURRENT_USER",
+        user 
+    }
+}
 
 export const login = (credentials, history) => {
     return dispatch => {
@@ -37,6 +43,30 @@ export const login = (credentials, history) => {
         return fetch("http://localhost:3001/api/v1/signup", {
             credentials: "include",
             method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            if(resp.error) {
+              alert(resp.error)
+            } else {
+                dispatch(setCurrentUser(resp.data))
+                history.push('/')
+
+            }
+        })
+        .catch(console.log)
+      }
+  }
+
+  export const logout = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/logout", {
+            credentials: "include",
+            method: "DELETE",
             headers: {
                 "Content-Type": "application/json"
             },
--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -2,6 +2,8 @@ export default (state = null, aciton) => {
     switch (aciton.type) {
         case "SET_CURRENT_USER":
             return aciton.user
+        case "CLEAR_CURRENT_USER":
+            return null
         default:
             return state
     }
