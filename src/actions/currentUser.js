bed7726d39bd030f048dc00bb455a4eec9e10e6e
inkanimal <theatomproject@gmail.com>
2019-09-17 10:10:51 -0400

    create logout component

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -31,7 +31,31 @@ export const login = (credentials, history) => {
         .catch(console.log)
       }
   }
-  
+
+  export const signup = (credentials, history) => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/signup", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            if(resp.error) {
+              alert(resp.error)
+            } else {
+                dispatch(setCurrentUser(resp.data))
+                history.push('/')
+
+            }
+        })
+        .catch(console.log)
+      }
+  }
+
     export const getCurrentUser = () => {
         return dispatch => {
             return fetch("http://localhost:3001/api/v1/get_current_user", {
