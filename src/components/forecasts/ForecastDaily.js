eeaa54322287fbd5b2091d07a17bdd45fe0ff9f1
inkanimal <theatomproject@gmail.com>
2019-09-22 16:48:06 -0400

    css stuff

--- src/components/forecasts/ForecastDaily.js
+++ src/components/forecasts/ForecastDaily.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 
 const ForecastDaily = ({ forecastData }) => {
   const renderForecast = forecastData.map((forecast, index) => {
-    const { apparentTemperatureMax, apparentTemperatureMin, precipProbability, humidity, summary, temperatureMax, temperatureMin, time } = forecast
+    const {precipProbability, humidity, summary, temperatureMax, temperatureMin, time, dewPoint, sunriseTime, sunsetTime, pressure, visibility, cloudCover, windBearing, windSpeed, windGust } = forecast
     return (
       <div key={index} style={{ border: 'solid 1px black', padding: '12px', margin: '10px' }}>
         <div className="date">
@@ -15,6 +15,13 @@ const ForecastDaily = ({ forecastData }) => {
        
         <h4>Chance of Rain: {Math.round(precipProbability * 100)}%</h4>
         <h4>Humidity: {Math.round(humidity * 100)}%</h4>
+        <h4>Dew Point: {Math.round(dewPoint)}°</h4>
+      <p>Sunrise: {moment.unix(sunriseTime).format('HH:mm')}</p>
+      <p>Sunrise: {moment.unix(sunsetTime).format('HH:mm')}</p>
+      <p>Wind: {degToCompass(windBearing)} @ {Math.round(windSpeed)} mph | Gusting to: {Math.round(windGust)} mph</p>
+      <p>Pressure: {(pressure * 0.0295301).toFixed(2)} inHg</p>
+      <p>Visibility: {(visibility).toFixed(2)} Miles</p>
+      <p>Cloud Cover: {(cloudCover * 100)}%</p>
       </div>
     )
   })
@@ -27,5 +34,10 @@ const ForecastDaily = ({ forecastData }) => {
   )
 }
   
+function degToCompass(num) {
+  var val = Math.floor((num / 22.5) + 0.5);
+  var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+  return arr[(val % 16)];
+}
  
 export default ForecastDaily;
--- src/components/forecasts/ForecastHourly.js
+++ src/components/forecasts/ForecastHourly.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import moment from 'moment'
+import './ForecastHourly.css'
 
 const ForecastHourly = ({ forecastData }) => {
   const renderForecast = forecastData.map((forecast, index) => {
     const { apparentTemperature, precipProbability, humidity, summary, temperature, time } = forecast
     return (
       <div key={index} style={{ border: 'solid 1px black', padding: '12px', margin: '10px' }}>
+        <div className="time">
         <h3>{moment.unix(time).format('HH:mm')}</h3>
-        <h3>Status: {summary}</h3>
-        <h4>Temperature: {temperature}</h4>
-        <h4>Feels Like: {apparentTemperature}</h4>
-        <h4>Chance of Rain: {precipProbability}%</h4>
-        <h4>Humidity: {humidity}%</h4>
+        </div>
+        <div className="temp">
+           {Math.round(temperature)}°
+        </div>
+        <div className="feelslike" id="feelslike">
+             
+        <p className="feels">Feels Like: {Math.round(apparentTemperature)}° </p>
+       </div>
+        <h3>{summary}</h3>
+        
+        <h4>Feels Like: {Math.round(apparentTemperature)}°</h4>
+        <h4>Chance of Precipitation: {precipProbability * 100}%</h4>
+        <h4>Humidity: {humidity * 100}%</h4>
       </div>
     )
   })
